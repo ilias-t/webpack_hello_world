c1a0cc7bb25f59df9270029c2725b2d30c070fe8
Ilias Tsangaris <ilias@readme.io>
2023-03-21 12:04:48 -0600

    step 4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 
+import "./style.scss";
+
 console.log("Hello World!", uuidv4());
 
-// Next: Add some Sass...
+// Next: Add a plugin to analyze our bundle...
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
+const path = require("path");
+
 module.exports = {
+  mode: process.NODE_ENV || "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "blammo.js",
+  },
+  module: {
+    rules: [
+      {
+        // scss
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
